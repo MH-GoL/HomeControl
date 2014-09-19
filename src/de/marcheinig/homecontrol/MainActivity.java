81985eb107266b993f617275cc8a894e7731af64
MH-GoL <Marc_Heinig@gmx.de>
2014-09-19 22:08:21 +0200

    Neuer Knopf
    Neuen Button hinzugefügt

--- src/de/marcheinig/homecontrol/MainActivity.java
+++ src/de/marcheinig/homecontrol/MainActivity.java
@@ -4,14 +4,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
+		
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Toast.makeText(getApplicationContext(), "Hallo", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
